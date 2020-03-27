ca38129f843c958c8c4f9060b5018ab92439c70b
gonza <1_web9_20@iesjovellanos.org>
2020-03-27 16:43:53 +0100

    vista Registro hecha

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ Macaw::get($URL_PATH . '/', "controller\LobbyController@lobby");
 
 Macaw::get($URL_PATH . '/mesa/(:any)', "controller\MesaController@partida");
 
+Macaw::get($URL_PATH . '/registrate', "controller\UserController@registro");
+
 Macaw::get($URL_PATH . '/perfil/(:any)', "controller\UserController@perfil");
 
 Macaw::error(function() {
--- orm/ormChat.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace orm;
-use \dawfony\Klasto;
-class OrmChat {
-
-}
