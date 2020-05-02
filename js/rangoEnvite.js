96091e5391097af558e31d51f8a9fdadd729ea4b
Al-Gonzarismi <gonzaildue@gmail.com>
2020-05-02 19:10:23 +0200

    partida alpha

--- js/rangoEnvite.js
+++ js/rangoEnvite.js
@@ -1,6 +1,34 @@
 window.onload = function () {
-    $('#rangoEnvite').on('change', function() {
-        $('#textoEnvite').text($(this).val());
-        $('#envidar').text('Envido ' + $(this).val());
+    $('#mas').click(function () {
+        var num = Number($('#textoEnvidar').text())
+        if (num < mesa.puntos) {
+            var suma = num + 1;
+            $('#textoEnvidar').text(suma);
+            $('#envidar').text("Envido " + suma);
+        }
+    })
+    $('#menos').click(function () {
+        var num = Number($('#textoEnvidar').text())
+        if (num > 2) {
+            var suma = num - 1;
+            $('#textoEnvidar').text(suma);
+            $('#envidar').text("Envido " + suma);
+        }
+    })
+    $('#haymas').click(function () {
+        var num = Number($('#haytextoEnvidar').text())
+        if (num < mesa.puntos) {
+            var suma = num + 1;
+            $('#haytextoEnvidar').text(suma);
+            $('#hayenvidar').text("Envido " + suma);
+        }
+    })
+    $('#haymenos').click(function () {
+        var num = Number($('#haytextoEnvidar').text())
+        if (num > 2) {
+            var suma = num - 1;
+            $('#haytextoEnvidar').text(suma);
+            $('#hayenvidar').text("Envido " + suma);
+        }
     })
 }
